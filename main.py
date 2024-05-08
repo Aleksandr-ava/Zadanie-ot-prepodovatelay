f891fe84248b2509be2ac1ba3e947555ffdae6eb
Aleksandr-ava <167477534+Aleksandr-ava@users.noreply.github.com>
2024-05-08 10:30:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+            # Таблица умножения
+
+# print('Введите 4 числа через пробел для переумножения')
+# a, b, c, d = map(int, input().split())
+#
+# if (a, b, c, d < 10) or (a <= b) or (c <= d):
+#     print('', end='\t')
+#     for i in range(c, d + 1):
+#         print(i, end='\t')
+#     print('')
+#     for j in range(a, b + 1):
+#         print(j, end='\t')
+#         for i in range(c, d + 1):
+#             print(i * j, end='\t')
+#         print('')
+
+
+            # Стороны треуголника
+
+# a = int(input('Введите длину стороны 1 '))
+# b = int(input('Введите длину стороны 2 '))
+# c = int(input('Введите длину стороны 3 '))
+# print()
+#
+# if (a == b) and (b == c) and (c == a):
+#     print('равносторонний')
+# elif (a != b) and (b != c) and (c != a):
+#     print('разносторонний')
+# else:
+#     print('равнобедренный')
+
+
+            # Серединное знаение
+
+# a = int(input('Введите число 1 '))
+# b = int(input('Введите число 2 '))
+# c = int(input('Введите число 3 '))
+# print()
+#
+# if (a > b) and (a < c):
+#     print(a)
+# elif (b > a) and (b < c):
+#     print(b)
+# elif (c > a) and (c < b):
+#     print(c)
+
+
+            # Смешение цветов
+
+# a = input('Напишите цвет 1 ')
+# b = input('Напишите цвет 2 ')
+# print()
+#
+# if a == 'красный' and b == 'синий':
+#     print('фиолетовый')
+# elif a == 'синий' and b == 'красный':
+#     print('фиолетовый')
+# elif a == 'красный' and b == 'жёлтый':
+#     print('оранжевый')
+# elif a == 'жёлтый' and b == 'красный':
+#     print('оранжевый')
+# elif a == 'синий' and b == 'жёлтый':
+#     print('зелёный')
+# elif a == 'жёлтый' and b == 'синий':
+#     print('зелёный')
+# else:
+#     print('ошибка')
